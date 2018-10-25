effd22341fb7c81d0c8588e84deab8540eb94ed0
zyxpz <mythmxlcool@vip.qq.com>
2018-10-25 23:24:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# h5轮播图
+
+```js
+const carousel = new Carousel({
+		warp: 'warp', // 父元素
+		main: 'main', // 子元素
+		play: true, // 是否自动播放
+    point: true, // 是否带有小标
+    time: 2000, // 转换时间 默认2000
+    horizontal: true, // 是否横像
+    pointColor: 'blue', // 小点颜色
+    pointSize: '6px', // 圆点大小
+    touch: true, // 是否可滑动 默认不可以
+	})
+
+	carousel.init();
+```
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const plugins = require('gulp-load-plugins')();
+const uglify = require('gulp-uglify');
+const gutil = require('gulp-util');
+
+gulp.task('clean', () => {
+	return gulp.src('lib')
+		.pipe(plugins.clean({
+			force: true
+		}))
+});
+
+// 编译js
+gulp.task('js', () => {
+	return gulp.src(['./src/index.js'])
+		.pipe(babel())
+		.on('error', (err) => {
+			gutil.log(gutil.colors.red('[Error]'), err.toString());
+		})
+		.pipe(uglify())
+		.on('error', (err) => {
+			gutil.log(gutil.colors.red('[Error]'), err.toString());
+		})
+		.pipe(gulp.dest('lib'))
+});
+
+// 监听js修改
+gulp.task('watch:js', () => {
+	gulp.watch(['./src/index.js']);
+});
+
+gulp.task('default', ['js', 'watch:js']);
+
+gulp.task('build', ['clean', 'default']);
